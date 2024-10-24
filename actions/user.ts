f939a1184f089a987c49c4e835c8ea6055f59ea2
sendistephen <sendi.stev@gmail.com>
2024-10-24 16:45:13 +0300

    feat: Add function to handle user authentication and management on server context

--- actions/user.ts
+++ actions/user.ts
@@ -1,11 +1,70 @@
 "use server";
 
 import { currentUser } from "@clerk/nextjs/server";
+import { prisma } from "@/lib/prisma";
 
 export const onAuthenticatedUser = async () => {
   try {
     // check if clerk user exists
     const user = await currentUser();
     if (!user) return { status: 404 };
-  } catch (error) {}
+
+    const foundUser = await prisma.user.findUnique({
+      where: {
+        clerkId: user.id,
+      },
+      include: {
+        workspace: {
+          where: {
+            User: {
+              clerkId: user.id,
+            },
+          },
+        },
+      },
+    });
+    if (foundUser) return { status: 200, user: foundUser };
+    // create a new user in the database
+    const newUser = await prisma.user.create({
+      data: {
+        clerkId: user.id,
+        email: user.emailAddresses[0].emailAddress,
+        firstname: user.firstName,
+        lastname: user.lastName,
+        image: user.imageUrl,
+        studio: {
+          create: {}, //uses all the default values
+        },
+        subscription: {
+          create: {},
+        },
+        workspace: {
+          create: {
+            name: `${user.firstName}'s Workspace`,
+            type: "PERSONAL",
+          },
+        },
+      },
+      include: {
+        workspace: {
+          where: {
+            User: {
+              clerkId: user.id,
+            },
+          },
+        },
+        subscription: {
+          select: {
+            plan: true,
+          },
+        },
+      },
+    });
+    if (newUser) {
+      return { status: 201, user: newUser };
+    }
+    return { status: 400 };
+  } catch (error) {
+    return { status: 500 };
+  }
 };
